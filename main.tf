07d62e567a68cdddabc0b133a4452f41e5c11e66
mmr116 <38760695+mmr116@users.noreply.github.com>
2024-05-21 17:20:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "azurerm_key_vault" "example" {
   name                = var.keyvault_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  tenant_id           = var.ARM_TENANT_ID
   sku_name            = var.keyvault_sku_name
 
   soft_delete_retention_days = 7  # Soft delete retention days; default is 90 days, set as needed
